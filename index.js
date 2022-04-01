7d448e1205ca65147b4b862b8ffe3a3d0735528f
pdavisDU <pdavisdu@gmail.com>
2022-04-01 16:16:07 -0600

    comment

---
+++ index.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const mgmt = require('./lib/manager');
+const eng = require('./lib/engineer');
+const intr = require('./lib/intern');
