ea73eeaf81ef9aa324be9de7f8eb8b3709fdd326
pdavisDU <pdavisdu@gmail.com>
2022-07-26 11:37:20 -0600

    fixed ui error

--- index.js
+++ index.js
@@ -1,113 +1,114 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
 // const Employee = require('./lib/Employee');
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
 const teams = [];
 
 const mgmtQues = [
-    {
-        message: 'Manager name...',
-        name:'mName',
-        type: 'input',
-    },
-    {
-        message: 'Manager ID...',
-        name: 'mId',
-        type: 'input',
-    },
-    {
-        message: 'Manager E-mail...',
-        name: 'mEmail',
-        type: 'input',
-    },
-    {
-        message: 'Managers office number...',
-        name: 'mOffice',
-        type: 'input',
-    },
+  {
+    message: "Manager name...",
+    name: "mName",
+    type: "input",
+  },
+  {
+    message: "Manager ID...",
+    name: "mId",
+    type: "input",
+  },
+  {
+    message: "Manager E-mail...",
+    name: "mEmail",
+    type: "input",
+  },
+  {
+    message: "Managers office number...",
+    name: "mOffice",
+    type: "input",
+  },
 ];
 
 const emplyQues = [
-    {
-        message: 'Do you want to add an Engineer or Intern?',
-        name: 'role',
-        type: 'list',
-        choices: ['Engineer', 'Intern'],
+  {
+    message: "Do you want to add an Engineer or Intern?",
+    name: "role",
+    type: "list",
+    choices: ["Engineer", "Intern"],
+  },
+  {
+    message: (input) => `What is ${input.role} name`,
+    name: "Name",
+    type: "input",
+  },
+  {
+    message: (input) => `What is ${input.role} ID`,
+    name: "Id",
+    type: "input",
+  },
+  {
+    message: (input) => `What is ${input.role} Email`,
+    name: "Email",
+    type: "input",
+  },
+  {
+    message: (input) => {
+      if (input.role === "Engineer") {
+        return `What is ${input.role} Github username`;
+      } else {
+        return `What school did ${input.role} go/goes to`;
+      }
     },
-    {
-        message: (input)=> `What is ${input.role} name`,
-        name: 'Name',
-        type: 'input',
-    },
-    {
-        message: (input)=> `What is ${input.name} ID`,
-        name: 'Id',
-        type: 'input',
-    },
-    {
-        message: (input)=> `What is ${input.name} Email`,
-        name: 'Email',
-        type: 'input',
-    },
-    {
-        message: (input)=> {
-            if (input.role === 'Engineer') {
-                return `What is ${input.name} Github username`;
-            } else {
-                return `What school did ${input.name} go/goes to`;
-            }
-        },
-        name: 'other',
-        type: 'input',
-    },
+    name: "other",
+    type: "input",
+  },
 ];
 start();
 
 function start() {
-    inquirer.prompt(mgmtQues).then(input => {
-        const man = new Manager(
-            input.mName,
-            input.mId,
-            input.mEmail,
-            input.mOffice
-        );
-        teams.push(man);
-        console.log(`${input.mName} added`);
-        eAdd();
-    });
+  inquirer.prompt(mgmtQues).then((input) => {
+    const man = new Manager(
+      input.mName,
+      input.mId,
+      input.mEmail,
+      input.mOffice
+    );
+    teams.push(man);
+    console.log(`${input.mName} added`);
+    eAdd();
+  });
 }
-
+// eAdd();
 function eAdd() {
-    inquirer.prompt([
-        {
-        message: 'Add another?',
-        name: 'addEE',
-        type: 'confirm',
-    }
+  inquirer
+    .prompt([
+      {
+        message: "Add another?",
+        name: "addEE",
+        type: "confirm",
+      },
     ])
-    .then(input => {
-       input.addEE ? addEmp() : showHTML();
+    .then((input) => {
+      input.addEE ? addEmp() : showHTML();
     });
 }
 
-function addEmp(){
-    inquirer.prompt(emplyQues).then(input=>{
-        if(input.role === "Engineer"){
-            const ee = new Engineer(input.name, input.id, input.other);
-            teams.push(ee);
-        } else {
-            const ee = new Intern(input.name, input.id, input.email, input.other);
-            teams.push(ee);
-        }
-        console.log("Added")
-    })
+function addEmp() {
+  inquirer.prompt(emplyQues).then((input) => {
+    if (input.role === "Engineer") {
+      const ee = new Engineer(input.name, input.id, input.other);
+      teams.push(ee);
+    } else {
+      const ee = new Intern(input.name, input.id, input.email, input.other);
+      teams.push(ee);
+    }
+    console.log("Added");
+  });
 }
 
-function showHTML(){
-    const html = `
+function showHTML() {
+  const html = `
     <!DOCTYPE html>
         <html lang="en">
           <head>
@@ -128,13 +129,18 @@ function showHTML(){
             </header>
             <main class="container">
             <div class="card-box row justify-content-center">
-            ${teams.map(Employee => Employee.createHTML(Employee.officeNumber || Employee.github || Employee.school)).join("\n")}
+            ${teams
+              .map((Employee) =>
+                Employee.createHTML(
+                  Employee.officeNumber || Employee.github || Employee.school
+                )
+              )
+              .join("\n")}
             </div>
             </main>
             </body>
             </html>
     `;
-    fs.writeFileSync("./dist/index.html", html);
-    console.log('page created');
+  fs.writeFileSync("./dist/index.html", html);
+  console.log("page created");
 }
-
